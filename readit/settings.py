651babcb87ed1a22be7ea8b9d3530a6f935160ec
Ahmed Nader <ahmednader111@gmail.com>
2021-06-02 23:13:43 +0200

    Added requirements file

--- readit/settings.py
+++ readit/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2z!*y(j6y-ktb4q@47dd9=cps0f3u+6(wsvkn+ssq*bt+d08#6'
+SECRET_KEY = os.environ.get('SECRET_KEY') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["readit-api.herokuapp.com", "127.0.0.1"]
 
 
 # Application definition
